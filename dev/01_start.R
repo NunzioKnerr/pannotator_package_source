7b4e95793407f7705777271abb8503696e822af8
kne01b <Nunzio.Knerr@csiro.au>
2026-03-16 12:00:47 +1100

    updated version number to comply with CRAN requirements

--- dev/01_start.R
+++ dev/01_start.R
@@ -47,7 +47,7 @@ golem::fill_desc(
            role = "cph")
   ),
   repo_url = "https://github.com/NunzioKnerr/pannotator_package_source", # The URL of the GitHub repo (optional),
-  pkg_version = "1.0.0.6", # The version of the package containing the app
+  pkg_version = "1.0.1", # The version of the package containing the app
   set_options = TRUE # Set the global golem options
 )
 
